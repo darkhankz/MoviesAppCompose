d4b95a03f0ed3fae02c4ef801e21e4d11c38bc25
darkhankz <upstudynow@gmail.com>
2023-02-14 22:11:31 +0600

    create Splash screen

---
+++ app/src/main/java/com/example/moviesappcompose/utils/Constans.kt
@@ -0,0 +1,9 @@
+package com.example.moviesappcompose.utils
+
+class Constans {
+    object Screens{
+        const val SPLASH_SCREEN = "splash screen"
+        const val MAIN_SCREEN = "main screen"
+        const val DETAILS_SCREEN = "details screen"
+    }
+}
