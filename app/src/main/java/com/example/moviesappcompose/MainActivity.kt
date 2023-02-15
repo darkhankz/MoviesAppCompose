5cd304045b64ff194705af5bf4bc1bd90d912a04
darkhankz <upstudynow@gmail.com>
2023-02-15 12:49:56 +0600

    create retrofit

--- app/src/main/java/com/example/moviesappcompose/MainActivity.kt
+++ app/src/main/java/com/example/moviesappcompose/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.example.moviesappcompose.navigation.SetupNavHost
 import com.example.moviesappcompose.ui.theme.MoviesAppComposeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
